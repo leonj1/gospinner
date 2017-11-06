478fe6411315fd7a58533a1f759a704a04d53f9a
Jose Leon <jleon43@bloomberg.net>
2017-11-06 08:47:26 -0500

    Adding unit test wrapper

---
+++ doit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# create the file with random words just once since its time consuming
+if [ ! -f input.txt ]; then
+	python iter.py
+fi
+
+# Unit test showing where things fail
+go test -run TestCallingSetMessageLotsOfTimes
+
